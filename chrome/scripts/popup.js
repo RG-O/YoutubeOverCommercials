d49ca9b3dc769f4cd087b88bf8a1038d16dc178e
RG-O <113465206+RG-O@users.noreply.github.com>
2024-05-10 10:39:35 -0400

    Incompatible Firefox Code to Chrome

--- chrome/scripts/popup.js
+++ chrome/scripts/popup.js
@@ -1,4 +1,3 @@
-//TODO: hide ID text fields when corresponding radio button not checked
 
 //grab all user set values
 chrome.storage.sync.get([
@@ -15,7 +14,13 @@ chrome.storage.sync.get([
     'overlayVideoLocationVertical',
     'mainVideoVolumeDuringCommercials',
     'mainVideoVolumeDuringNonCommercials',
-    'shouldHideYTBackground'
+    'shouldHideYTBackground',
+    'commercialDetectionMode',
+    'mismatchCountThreshold',
+    'matchCountThreshold',
+    'colorDifferenceMatchingThreshold',
+    'manualOverrideCooldown',
+    'isDebugMode'
 ], (result) => {
 
     //set them to default if not set by user yet
@@ -27,12 +32,30 @@ chrome.storage.sync.get([
     optionsForm.otherLiveURL.value = result.otherLiveURL ?? 'https://tv.youtube.com/watch/_2ONrjDR7S8';
     optionsForm.overlayVideoLocationHorizontal.value = result.overlayVideoLocationHorizontal ?? 'middle';
     optionsForm.overlayVideoLocationVertical.value = result.overlayVideoLocationVertical ?? 'middle';
-    optionsForm.mainVideoFade.value = result.mainVideoFade ?? 30;
+    optionsForm.mainVideoFade.value = result.mainVideoFade ?? 50;
     optionsForm.videoOverlayWidth.value = result.videoOverlayWidth ?? 75;
     optionsForm.videoOverlayHeight.value = result.videoOverlayHeight ?? 75;
     optionsForm.mainVideoVolumeDuringCommercials.value = result.mainVideoVolumeDuringCommercials ?? 0;
     optionsForm.mainVideoVolumeDuringNonCommercials.value = result.mainVideoVolumeDuringNonCommercials ?? 100;
     optionsForm.shouldHideYTBackground.checked = result.shouldHideYTBackground ?? true;
+    optionsForm.commercialDetectionMode.value = result.commercialDetectionMode ?? 'auto';
+    optionsForm.mismatchCountThreshold.value = result.mismatchCountThreshold ?? 2;
+    optionsForm.matchCountThreshold.value = result.matchCountThreshold ?? 2;
+    optionsForm.colorDifferenceMatchingThreshold.value = result.colorDifferenceMatchingThreshold ?? 8;
+    optionsForm.manualOverrideCooldown.value = result.manualOverrideCooldown ?? 20;
+    optionsForm.isDebugMode.checked = result.isDebugMode ?? false;
+
+    document.getElementById(optionsForm.commercialDetectionMode.value).style.display = 'block';
+    const modeRadios = document.forms["optionsForm"].elements["commercialDetectionMode"];
+    for (let i = 0, max = modeRadios.length; i < max; i++) {
+        modeRadios[i].addEventListener('change', toggleModeInstructionsVisability);
+    }
+
+    document.getElementById(optionsForm.overlayVideoType.value).style.display = 'block';
+    const videoTypeRadios = document.forms["optionsForm"].elements["overlayVideoType"];
+    for (let i = 0, max = videoTypeRadios.length; i < max; i++) {
+        videoTypeRadios[i].addEventListener('change', toggleIDFieldVisability);
+    }
     
 });
 
@@ -55,7 +78,12 @@ document.getElementById("save-button").onclick = function () {
         optionsForm.videoOverlayWidth.value &&
         optionsForm.videoOverlayHeight.value &&
         optionsForm.mainVideoVolumeDuringCommercials.value &&
-        optionsForm.mainVideoVolumeDuringNonCommercials.value
+        optionsForm.mainVideoVolumeDuringNonCommercials.value &&
+        optionsForm.commercialDetectionMode.value &&
+        optionsForm.mismatchCountThreshold.value &&
+        optionsForm.matchCountThreshold.value &&
+        optionsForm.colorDifferenceMatchingThreshold.value &&
+        optionsForm.manualOverrideCooldown.value
     ) {
 
         let overlayHostName;
@@ -83,7 +111,13 @@ document.getElementById("save-button").onclick = function () {
             videoOverlayHeight: optionsForm.videoOverlayHeight.value,
             mainVideoVolumeDuringCommercials: optionsForm.mainVideoVolumeDuringCommercials.value,
             mainVideoVolumeDuringNonCommercials: optionsForm.mainVideoVolumeDuringNonCommercials.value,
-            shouldHideYTBackground: optionsForm.shouldHideYTBackground.checked
+            shouldHideYTBackground: optionsForm.shouldHideYTBackground.checked,
+            commercialDetectionMode: optionsForm.commercialDetectionMode.value,
+            mismatchCountThreshold: optionsForm.mismatchCountThreshold.value,
+            matchCountThreshold: optionsForm.matchCountThreshold.value,
+            colorDifferenceMatchingThreshold: optionsForm.colorDifferenceMatchingThreshold.value,
+            manualOverrideCooldown: optionsForm.manualOverrideCooldown.value,
+            isDebugMode: optionsForm.isDebugMode.checked
         }, function () {
             window.close();
             alert("Changes saved successfully! Note: May need to refresh page to have all updates take effect.");
@@ -102,10 +136,33 @@ document.getElementById("expand-button").onclick = function () {
     this.classList.toggle("button-hidden");
     var content = this.nextElementSibling;
     content.style.display = "block";
+    //reduce size of elements to account for firefox scroll bar showing over fields
+    document.getElementsByTagName('form')[0].style.width = '288px';
+    document.getElementsByTagName('h1')[0].style.fontSize = '35px';
 }
 
 
 //close chrome extension window if they click to close
 document.getElementById("close-button").onclick = function () {
     window.close();
+}
+
+
+//show/hide ID and URL fields when their corresponding radio button is checked/unchecked
+function toggleIDFieldVisability() {
+    let idFields = document.getElementsByClassName('id-field-wrapper');
+    for (let i = 0, max = idFields.length; i < max; i++) {
+        idFields[i].style.display = 'none';
+    }
+    document.getElementById(optionsForm.overlayVideoType.value).style.display = 'block';
+}
+
+
+//TODO: combine this function and one above for cleaner code
+function toggleModeInstructionsVisability() {
+    let modeInstructions = document.getElementsByClassName('commercial-detection-mode-instructions-wrapper');
+    for (let i = 0, max = modeInstructions.length; i < max; i++) {
+        modeInstructions[i].style.display = 'none';
+    }
+    document.getElementById(optionsForm.commercialDetectionMode.value).style.display = 'block';
 }
